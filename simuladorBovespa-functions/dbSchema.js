08621bd8e96c01b85573476160a9ade6ab8f7cde
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-05-21 17:15:59 +0000

    getTransactions

---
+++ simuladorBovespa-functions/dbSchema.js
@@ -0,0 +1,32 @@
+let db = {
+    transactions: {
+        commentCount: 0,
+        companyName: 'Petroleo Brasileiro S.A. - Petrobrás',
+        price: 18.05,
+        quantity: 10,
+        symbol: 'PETR4',
+        total: 180.5,
+        transactedAt: "2020-04-28T00:08:18.265Z",
+        type: 'Compra',
+        userId: 'YhY3wDwIErYjCVRjWRvaAFFJ4V92',
+    },
+    comments: {
+        body: 'Querendo ganhar os dividendos!',
+        createdAt: "2020-05-04T15:31:21.397Z",
+        transactionId: 'pcSRG8OOsUCk3SZdoqOO',
+        userId: 'YhY3wDwIErYjCVRjWRvaAFFJ4V92',
+        userImage: 'https://firebasestorage.googleapis.com/v0/b/simulador-b3.appspot.com/o/no-img.png?alt=media',
+        userName: 'CRIOS'
+    },
+    users: {
+        bio: 'CRIOS da UFRJ Macaé',
+        caixa: 9864.75,
+        createdAt: "2020-05-02T15:31:21.397Z",
+        email: 'ufrj.crios@gmail.com',
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/simulador-b3.appspot.com/o/no-img.png?alt=media',
+        lastName: 'UFRJ',
+        location: 'Campos, Brasil',
+        name: 'CRIOS',
+        website: 'https://facebook.com'
+    }
+}
--- simuladorBovespa-functions/functions/index.js
+++ simuladorBovespa-functions/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,17 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello world!");
 });
+
+exports.getTransactions = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('transactions').get()
+        .then(data => {
+            let transactions = []
+            data.forEach(doc => {
+                transactions.push(doc.data())
+        })
+        return res.json(transactions)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+})
