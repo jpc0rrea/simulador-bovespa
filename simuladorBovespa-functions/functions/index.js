3e8be982eefca5d86dfa3714fa6a20994b74616c
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-05-22 02:29:13 +0000

    buyOneSymbol function

--- simuladorBovespa-functions/functions/index.js
+++ simuladorBovespa-functions/functions/index.js
@@ -2,15 +2,12 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { getPrice } = require('./utils/yahooFinance')
 
+
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world!");
-});
-
 exports.getTransactions = functions.https.onRequest((req, res) => {
   admin
     .firestore()
