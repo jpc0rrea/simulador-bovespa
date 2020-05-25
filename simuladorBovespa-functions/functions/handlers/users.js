d828b1ee7b94018353ad9f0acf605f62ed6b7697
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-05-25 18:28:15 +0000

    refactoring

---
+++ simuladorBovespa-functions/functions/handlers/users.js
@@ -0,0 +1,98 @@
+const { db } = require("../utils/admin");
+
+const { config } = require("../utils/config");
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const {
+  validateSignupData,
+  validateLoginData,
+} = require("../utils/validators");
+
+exports.signup = (req, res) => {
+  let password = req.body.password;
+  let confirmPassword = req.body.confirmPassword;
+  let email = req.body.email;
+
+  const { valid, errors } = validateSignupData(newUser);
+
+  if (!valid) return res.status(400).json(errors);
+
+  const newUser = {
+    email,
+    password,
+    confirmPassword,
+  };
+
+  let token, userId;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        bio: "",
+        caixa: 10000,
+        createdAt: new Date().toISOString(),
+        email: newUser.email,
+        imageUrl: "",
+        lastName: "",
+        location: "",
+        name: "",
+        website: "",
+      };
+      return db.doc(`/users/${userId}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ message: "E-mail já está em uso." });
+      } else if (err.code === "auth/invalid-email") {
+        return res.status(400).json({ message: "E-mail inválido." });
+      } else if (err.code === "auth/weak-password") {
+        return res
+          .status(400)
+          .json({ message: "A senha deve conter 6 ou mais digitos" });
+      }
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+exports.login = (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  const { valid, errors } = validateLoginData(user);
+
+  if (!valid) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/wrong-password") {
+        return res.status(403).json({ message: "Senha incorreta." });
+      } else if (err.code === "auth/user-not-found") {
+        return res.status(403).json({ message: "Usuário não encontrado." });
+      }
+      return res.status(500).json({ error: err.code });
+    });
+};
---
+++ simuladorBovespa-functions/functions/utils/validators.js
@@ -0,0 +1,50 @@
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+exports.validateSignupData = (data) => {
+  let errors = {};
+
+  // Conferindo se o usuário escreveu algo no e-mail, senha e confirmação de senha
+  if (isEmpty(data.email)) {
+    errors.email = "Campo 'e-mail' não pode estar vazio";
+  } else if (!isEmail(data.email)) {
+    errors.email = "E-mail inválido";
+  }
+
+  if (isEmpty(data.password)) {
+    errors.password = "Campo 'senha' não pode estar vazio";
+  }
+
+  if (data.password !== data.confirmPassword) {
+    errors.confirmPassword = "Senhas devem ser iguais.";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+exports.validateLoginData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) errors.email = "Campo 'e-mail' não pode estar vazio";
+  if (isEmpty(data.password))
+    errors.password = "Campo 'senha' não pode estar vazio";
+  if (!isEmail(data.email)) {
+    errors.email = "E-mail inválido";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
