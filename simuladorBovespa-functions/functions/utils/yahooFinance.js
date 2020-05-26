00d04ba145d89c56a9a13fab69913a8fd2c3d2f5
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-05-25 22:15:23 -0300

    testando git no vscode

--- simuladorBovespa-functions/functions/utils/yahooFinance.js
+++ simuladorBovespa-functions/functions/utils/yahooFinance.js
@@ -1,39 +1,38 @@
-const https = require('https')
-
+const https = require("https");
 
 const getPrice = (symbol) => {
-    return new Promise((resolve, reject) => {
-        symbol = symbol.trim().toUpperCase()
-        if (symbol.search(/\.SA/) === -1) {
-            symbol += '.SA';
+  return new Promise((resolve, reject) => {
+    // symbol é o papel da empresa
+    symbol = symbol.trim().toUpperCase();
+    if (symbol.search(/\.SA/) === -1) {
+      symbol += ".SA";
+    }
+    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`;
+    https.get(url, (res) => {
+      let data = "";
+      let dataOfCompany;
+      let result = {};
+      res.on("data", (dados) => {
+        data += dados;
+      });
+
+      res.on("end", () => {
+        try {
+          dataOfCompany = JSON.parse(data).quoteResponse.result[0];
+          result.price = dataOfCompany.regularMarketPrice;
+          result.symbol = dataOfCompany.symbol.split(".")[0];
+          result.name = dataOfCompany.longName;
+          resolve(result);
+        } catch (e) {
+          reject(e);
         }
-        const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`;
-        https.get(url, (res) => {
-            let data = ""
-            let dataOfCompany;
-            let result = {}
-            res.on("data", (dados) => {
-                data += dados
-            });
-
-            res.on("end", () => {
-                try {
-                    dataOfCompany = JSON.parse(data).quoteResponse.result[0]
-                    result.price = dataOfCompany.regularMarketPrice
-                    result.symbol = dataOfCompany.symbol.split(".")[0]
-                    result.name = dataOfCompany.longName
-                    resolve(result)
-                } catch (e) {
-                    reject(e)
-                }
-            })
-        })
-
-    })
-}
+      });
+    });
+  });
+};
 
 const round = (number) => {
-    return Math.round(number * 100) / 100
-}
+  return Math.round(number * 100) / 100;
+};
 // getPrice("ITSA4").then((data) => console.log(data));
-module.exports = { getPrice, round }
+module.exports = { getPrice, round };
