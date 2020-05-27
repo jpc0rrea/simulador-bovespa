9cc4556a906270533cff2d74e069855207d1abd0
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-05-27 13:50:43 -0300

    add reduceUserDetails

--- simuladorBovespa-functions/functions/utils/validators.js
+++ simuladorBovespa-functions/functions/utils/validators.js
@@ -56,3 +56,19 @@ exports.validateLoginData = (data) => {
     valid: Object.keys(errors).length === 0 ? true : false,
   };
 };
+
+exports.reduceUserDetails = (data) => {
+  let userDetails = {};
+
+  if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+  if (!isEmpty(data.website.trim())) {
+    if (data.website.trim().substring(0, 4) !== "http") {
+      userDetails.website = `http://${data.website.tri()}`;
+    } else {
+      userDetails.website = data.website;
+    }
+  }
+  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+  return userDetails;
+};
